9b43f9d82f816621d5ce308f8142e5348fdfbec7
Archikoff <ak@archikoff.eu>
2021-05-23 15:56:38 +0300

    parser to def

--- ark_bot.py
+++ ark_bot.py
@@ -1,18 +1,17 @@
+import json
 import requests as rq
 from bs4 import BeautifulSoup as bs
 
-exams = rq.get('https://eteenindus.mnt.ee/public/vabadSoidueksamiajad.xhtml')
-content = exams.content
-html = content
-soup = bs(html, features="lxml")
-table = soup.find(id='eksami_ajad:kategooriaBEksamiAjad_data')
 
-tds = list(map(lambda td: td.get_text(), table.find_all('td')))
-ext_data = dict()
-for i in range(0, len(tds), 5):
-     ext_data[tds[i]] = tds[i+2:i+5]
+def parser():
+    exams = rq.get('https://eteenindus.mnt.ee/public/vabadSoidueksamiajad.xhtml')
+    content = exams.content
+    html = content
+    soup = bs(html, features="lxml")
+    table = soup.find(id='eksami_ajad:kategooriaBEksamiAjad_data')
 
-# print(ext_data)
-print(ext_data.keys(), ext_data.values())
-
-# собрать оба фора в один класс. Создать отдельный класс, который возвращает города + 3 времени?
+    tds = list(map(lambda td: td.get_text(), table.find_all('td')))
+    ext_data = dict()
+    for i in range(0, len(tds), 5):
+        ext_data[tds[i]] = tds[i+2:i+5]
+    return ext_data
